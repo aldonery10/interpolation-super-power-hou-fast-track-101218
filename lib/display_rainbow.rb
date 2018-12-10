347a05738ee9dbb7cbd302d4aff0c809ab24ba5d
nery.aldo <nery.aldo@outlook.com>
2018-12-10 06:01:01 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
-# Write your #display_rainbow method here
+def display_rainbow
+  puts display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  
+end
+  
+  "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  
